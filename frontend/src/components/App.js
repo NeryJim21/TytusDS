cc652378a62b60c6bf8dda92123bfdd14d60d1ce
Miguel Guirola <3002728550101@ingenieria.usac.edu.gt>
2021-06-15 19:49:57 -0600

    Build p5

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -15,10 +15,10 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/TytusDS/" component={Home} />
-          <Route exact path="/TytusDS/Lineales" component={Lineal} />
-          <Route exact path="/TytusDS/Ordenamientos" component={Ordenamiento} />
-          <Route exact path="/TytusDS/Arboreas" component={Arborea} />
+          <Route exact path="." component={Home} />
+          <Route exact path="./Lineales" component={Lineal} />
+          <Route exact path="./Ordenamientos" component={Ordenamiento} />
+          <Route exact path=".Arboreas" component={Arborea} />
           <Route exact path="/TytusDS/Lineales/EnlazadaSimple" component={LinealEC} />
           <Route exact path="/TytusDS/Lineales/CircularDoble" component={LinealEC} />
           <Route exact path="/TytusDS/Lineales/Pila" component={LinealPC} />
